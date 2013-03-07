d096bc2eec07f165065757295ef5dc51a9ab7197
Paul Knox-Kennedy <paul@knox-kennedy.me.uk>
2013-03-07 20:21:06 +0000

    Got a character

--- test/test.c
+++ test/test.c
@@ -17,41 +17,69 @@ int *mouse;
 struct joystick_t *joy1;
 
 texture_font_t *font;
+texture_atlas_t *atlas;
 
-char * vert = "attribute vec3		vert_attrib;\
-attribute vec2		uv_attrib;\
-\
-uniform mat4		opm_uniform;\
-\
-varying vec2		v_frag_uv;\
-\
-void main(void) {\
-\
-	v_frag_uv = uv_attrib;\
-    gl_Position = opm_uniform * vec4(vert_attrib,1);\
-\
-}\
-";
+char * vert = 
+  "attribute vec3		vposition;\n"
+  "attribute vec2		uv_attrib;\n"
+  "varying vec2		        v_frag_uv;\n"
+  "void main(void) {\n"
+  "       v_frag_uv = uv_attrib;\n"
+  "       gl_Position = vec4(vposition,1);\n"
+  "}\n";
 
-char * frag = "uniform sampler2D		texture_uniform;\
-uniform float cx,cy;\
-varying vec2 v_frag_uv;\
- \
-\
-void main()\
-{\
-	float x=cx+v_frag_uv.x;\
-	float y=cy+v_frag_uv.y;\
-    gl_FragColor = texture2D(texture_uniform, vec2(x,y));\
-}\
-";
+
+char * frag = 
+  "precision mediump float;\n"
+  "uniform sampler2D		texture_uniform;\n"
+  "varying vec2 v_frag_uv;\n"
+  "void main()\n"
+  "{\n"
+  "    gl_FragColor = texture2D(texture_uniform, v_frag_uv);\n"
+  "}\n";
 
 int programHandle;
+int vertexHandle, texHandle, samplerHandle;
 
 void render() {
 
 
     texture_glyph_t *tgp = texture_font_get_glyph( font, 'A' );
+
+    GLfloat vVertices[] = { -0.1f, -0.1f, 0.0f, 
+                            0.1f, 0.1f, 0.0f,
+                            -0.1f, 0.1f, 0.0f,
+                            -0.1f, -0.1f, 0.0f,
+                            0.1f, -0.1f, 0.0f,
+                            0.1f, 0.1f, 0.0f};
+    GLfloat tVertices[] = { tgp->s0,tgp->t1,
+                            tgp->s1,tgp->t0,
+                            tgp->s0,tgp->t0,
+                            tgp->s0,tgp->t1,
+                            tgp->s1,tgp->t1,
+                            tgp->s1,tgp->t0 };
+
+
+   // Clear the color buffer
+   glClear ( GL_COLOR_BUFFER_BIT );
+
+   // Use the program object
+   glUseProgram ( programHandle );
+
+   // Load the vertex data
+   glVertexAttribPointer ( vertexHandle, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
+   glEnableVertexAttribArray ( 0 );
+   glVertexAttribPointer ( texHandle, 2, GL_FLOAT, GL_FALSE, 0, tVertices );
+   glEnableVertexAttribArray ( 1 );
+
+   glActiveTexture( GL_TEXTURE0 );
+   glBindTexture( GL_TEXTURE_2D, atlas->id );
+
+   glUniform1i ( samplerHandle, 0);
+
+   glDrawArrays ( GL_TRIANGLES, 0, 6 );
+
+   swapBuffers();
 
 }
 
@@ -59,17 +87,6 @@ int main(int argc, char **argv) {
   int vHandle, fHandle, length, compile_ok;
   /* Text to be printed */
   wchar_t *text = L"A Quick Brown Fox Jumps Over The Lazy Dog 0123456789";
-
-  /* Texture atlas to store individual glyphs */
-  texture_atlas_t *atlas = texture_atlas_new( 512, 512, 1 );
-
-  font = texture_font_new( atlas, "./fonts/Vera.ttf", 16 );
-
-  /* Cache some glyphs to speed things up */
-  texture_font_load_glyphs( font, L" !\"#$%&'()*+,-./0123456789:;<=>?"
-			    L"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
-			    L"`abcdefghijklmnopqrstuvwxyz{|}~");
-
 
 
     // creates a window and GLES context
@@ -91,8 +108,22 @@ int main(int argc, char **argv) {
     glEnable(GL_CULL_FACE);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glDisable(GL_BLEND);
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(0, 0.5, 1, 1);
+    //glEnable(GL_DEPTH_TEST);
+    glClearColor(0.5, 0.5, 0.5, 1);
+
+
+  /* Texture atlas to store individual glyphs */
+  atlas = texture_atlas_new( 512, 512, 3 );
+
+  font = texture_font_new( atlas, "./fonts/Vera.ttf", 30 );
+
+  /* Cache some glyphs to speed things up */
+  texture_font_load_glyphs( font, L" !\"#$%&'()*+,-./0123456789:;<=>?"
+			    L"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+			    L"`abcdefghijklmnopqrstuvwxyz{|}~");
+
+
+
 
     vHandle = glCreateShader(GL_VERTEX_SHADER);
     length = strlen(vert);
@@ -123,6 +154,7 @@ int main(int argc, char **argv) {
     glAttachShader(programHandle, vHandle);
     glAttachShader(programHandle, fHandle);
 
+
     glLinkProgram(programHandle);
 
     glGetProgramiv(programHandle, GL_LINK_STATUS, &compile_ok);
@@ -132,7 +164,12 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
-
+    
+    // Bind vPosition to attribute 0
+    vertexHandle = glGetAttribLocation ( programHandle, "vposition" );
+    texHandle = glGetAttribLocation ( programHandle, "uv_attrib" );
+    samplerHandle = glGetUniformLocation( programHandle, "texture_uniform" );
+    printf("%d %d %d\n", vertexHandle, texHandle, samplerHandle);
 
     texture_atlas_upload(atlas);
 
